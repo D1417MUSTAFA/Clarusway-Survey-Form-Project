98dd3f6db2a03de4cd847942dc13e5e95d902bc7
D1417MUSTAFA <musabdurak7@gmail.com>
2022-01-28 17:56:32 +0100

    all my practices

---
+++ App.js
@@ -0,0 +1,37 @@
+//console.log("i`m from file");
+
+var x = 7;
+var y = 5;
+
+console.log(x);
+console.log({y});
+y = x
+console.log({y});
+
+var a = 5;
+console.log(a);
+var a= 7;
+console.log(a);
+
+const h ="constant varibale";
+console.log(h);
+h = "2"
+
+console.log(d);var d;
+
+
+var f =;
+console.log(typof f);
+
+f = 5;
+console.log(typeof f);
+''
+f= "Merhaba ben string"
+console.log(typeof f);
+
+var merhaba = "merhaba"
+var selam = "sanada selam"
+var d = `merhaba """"""""""""""""??´´´´´´´´´´´´´´dunya `
+
+console.log(Boolean(""));
+console.log();
